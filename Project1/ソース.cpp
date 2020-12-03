ea10baae707b0812ee59075aa356bf80f6bbae3f
riobaba_neec <k020g1061@g.neec.ac.jp>
2020-12-03 09:47:06 +0900

    プロジェクト ファイルを追加します。

---
+++ Project1/ソース.cpp
@@ -0,0 +1,14 @@
+int cache = rand() % 3000;
+
+switch (cache / 1000)
+{
+case 0:
+	return (double)120;
+	break;
+case 1:
+	return (double)((762 - (245 / 2)) / 2);
+	break;
+case 2:
+	return (double)762 - 215;
+	break;
+}
